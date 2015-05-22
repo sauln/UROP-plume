fb5f7641e0b513127e07faaa81f63ae730bd020e
Nathaniel Saul <sauln@hawaii.edu>
2015-05-22 10:40:05 -1000

    removing icra draft and duplicate sim
    trying to clean up an old git learning mess up.

--- plume2.0/plume/cython/profiler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python -m cProfile -o profile/profile $1.py
-python profile/profileReader.py
-
--- plume2.0/plume/profiles/profileReader.py
+++
@@ -1,7 +0,0 @@
-import pstats
-
-
-p = pstats.Stats('profiles/profile')
-
-p.sort_stats('time').print_stats(5)
-
